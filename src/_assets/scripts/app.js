00dd6692f1ce7c474e75b44853666694169274d5
Kelly Anne Pipe <kellyanne.pipe@gmail.com>
2021-01-17 21:04:59 -0500

    Add a good success message for the form.

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -11,10 +11,50 @@ const processForm = form => {
       body: data,
     })
     .then(() => {
-      form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+      form.innerHTML = `
+      <div class="bg-green-100 rounded-md p-3 flex">
+      <svg
+          class="stroke-2 stroke-current text-green-600 h-8 w-8 mr-2 flex-shrink-0"
+          viewBox="0 0 24 24"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+      >
+          <path d="M0 0h24v24H0z" stroke="none" />
+          <circle cx="12" cy="12" r="9" />
+          <path d="M9 12l2 2 4-4" />
+      </svg>
+  
+      <div class="text-green-700">
+          <div class="font-bold text-xl">Thank you!</div>
+  
+          <div>We will be in touch soon! Looking forward to working with you!</div>
+      </div>
+      </div>
+      `;
     })
     .catch(error => {
-      form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+      form.innerHTML = `
+      <div class="bg-red-100 rounded-md p-3 flex">
+      <svg
+          class="stroke-2 stroke-current text-red-600 h-8 w-8 mr-2 flex-shrink-0"
+          viewBox="0 0 24 24"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+      >
+          <path d="M0 0h24v24H0z" stroke="none" />
+          <circle cx="12" cy="12" r="9" />
+          <path d="M9 12l2 2 4-4" />
+      </svg>
+  
+      <div class="text-red-700">
+          <div class="font-bold text-xl">There's been a problem with the form!</div>
+  
+          <div>Please try again at another time or reach out via email!  Sorry for the inconvenience.</div>
+      </div>
+      </div>
+      `;
     })
   }
 
